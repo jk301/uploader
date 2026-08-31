e3eb68e708497e969bf970b21a670e9ce7909c2d
junaid <jk7948677@gmail.com>
2026-08-31 23:07:08 +0530

    Proper route handlers

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,26 +1,106 @@
 // controllers/indexController.js
 
-function getMain(req, res) {
-    return res.render('index')
+const { prisma } = require('../lib/prisma.js')
+const utils = require('../lib/utils.js')
+
+async function getMain(req, res) {
+    // for testing
+    const user = req.user
+    if (user) {
+        return res.render('index', { logged: true, user: user})
+    } else {
+        return res.render('index')
+    }
+}
+
+function getRegister (req, res) {
+    res.render('register')
+}
+
+function getLogin (req, res) {
+    return res.render('login')
 }
 
 function getProtected (req, res) {
     return res.render('protected')
 }
 
-// run passport.authenticate('local') mid
-function postLogin (req, res) {
-    // redirect 
+async function postRegister (req, res) {
+    const password = req.body.password
+    const cnfrm_pass = req.body.cnfrm_pass
+    const username = req.body.username
+
+    if (password !== cnfrm_pass) {
+        return res.render('register', {
+            alerts: ["Passwords don't match."],
+            username,
+            password,
+            cnfrm_pass
+        })
+    }
+
+    try {
+        const hash = await utils.passGen(password)
+
+        const result = await prisma.user.create({
+            data: {
+                username: username,
+                hash: hash
+            }
+        })
+
+        if (result) {
+            return res.redirect('/login')
+        } else {
+            return res.render('register', {
+                alerts: ["Couldn't add user for some reason"]
+            })
+        }
+
+    } catch (err) {
+
+        let msg = 'Something went wrong'
+
+        if (err.code === "P2002") {
+            msg = 'Username is already taken.'
+        }
+
+        return res.render('register', {
+            alerts: [msg],
+            username
+        })
+    }
+
+}
+
+// run passport.authenticate('local') mid handles all data (cool)
+async function postLogin (req, res) {
+    res.redirect('/')    
 }
 
 function postLogout (req, res) {
-    // req.logout() or something
+    req.logout( err => {
+        if (err) {
+            return res.render('index', { alert: ['Logout failed'] })
+        }
+
+        req.session.destroy( err => {
+            if (err) {
+                return res.render('index', { alert: ['Logout failed'] })
+            }
+            res.clearCookie('connect.sid')
+            return res.redirect('/')
+        })
+    })
 }
 
 
 module.exports = {
     getMain,
+    getRegister,
+    getLogin,
     getProtected,
+    postRegister,
     postLogin,
     postLogout
 }
