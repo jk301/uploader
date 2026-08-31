136d9b1d17931ffd2591ab47f62f0ab488ee1b7c
junaid <jk7948677@gmail.com>
2026-08-31 20:27:15 +0530

    Added few routes - WIP

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,8 +8,19 @@ function getProtected (req, res) {
     return res.render('protected')
 }
 
+// run passport.authenticate('local') mid
+function postLogin (req, res) {
+    // redirect 
+}
+
+function postLogout (req, res) {
+    // req.logout() or something
+}
+
 
 module.exports = {
     getMain,
-    getProtected
+    getProtected,
+    postLogin,
+    postLogout
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,18 @@
-// routes/indRouter.js
+// routes/indexRouter.js
 
 const { Router } = require('express')
 const indexController = require('../controllers/indexController.js')
+const passport = require('passport')
 
 const indexRouter = Router()
 
 indexRouter.get('/', indexController.getMain)
+
+// run req.isAuthenticated() for gating routes
 indexRouter.get('/protected', indexController.getProtected)
+
+indexRouter.post('/login', passport.authenticate('local'), indexController.postLogin)
+indexRouter.post('/logout', indexController.postLogout)
+
 
 module.exports = indexRouter
