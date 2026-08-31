429bfd0fc0c1bdee3fe198210293fbb1544eb258
junaid <jk7948677@gmail.com>
2026-08-31 23:08:41 +0530

    main pages & their routes (auth on login)

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,10 +7,13 @@ const passport = require('passport')
 const indexRouter = Router()
 
 indexRouter.get('/', indexController.getMain)
+indexRouter.get('/register', indexController.getRegister)
+indexRouter.get('/login', indexController.getLogin)
 
 // run req.isAuthenticated() for gating routes
-indexRouter.get('/protected', indexController.getProtected)
+// indexRouter.get('/protected', indexController.getProtected)
 
+indexRouter.post('/register', indexController.postRegister)
 indexRouter.post('/login', passport.authenticate('local'), indexController.postLogin)
 indexRouter.post('/logout', indexController.postLogout)
 
