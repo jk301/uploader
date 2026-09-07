b9171dc738884a5a7441f0efd5e7337f2f1e5de1
junaid <jk7948677@gmail.com>
2026-09-07 23:21:07 +0530

    Added styling to main page & navbar

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
